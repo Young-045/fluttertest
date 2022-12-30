30d13d2f3840465b40203ba8c41730a149a792d7
yangpeixuan <yangpeixuan@aecg.com>
2022-12-30 13:01:33 +0800

    pack5

--- lib/charts/cartesian_chart.dart
+++ lib/charts/cartesian_chart.dart
@@ -38,7 +38,7 @@ class _CartesianChartState extends State<CartesianChart> {
         primaryXAxis: CategoryAxis(),
         // Chart title
         title: ChartTitle(
-            text: '66666',
+            text: '777878',
             backgroundColor: Colors.lightBlue,
             borderColor: Colors.green,
             borderWidth: 2,
